e3058e87441827b523db7f6de289a8e9e31ffc12
Ashwin Narayan <ashwinnarayan1994@gmail.com>
2024-11-10 09:53:54 +0800

    Reactive navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { IoMdMenu } from "react-icons/io";
 // import Image from 'next/image'
 
 type Props = {
@@ -12,14 +13,19 @@ export default function Navbar(props: Props) {
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="https://doctorsong16.github.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-            <span className="self-center text-2xl text-smoky-black font-ibarraRealNova whitespace-nowrap">dundu does stuff</span>
+            <span className="self-center font-semibold text-2xl text-smoky-black font-ibarraRealNova whitespace-nowrap">dundu does stuff</span>
         </a>
+        {/* Button to open the menu on mobile.*/}
+        <button className='inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden' type='button'>
+            <IoMdMenu className='text-smoky-black text-2xl' />
+        </button>
 
+        {/* On desktop, we just use a navbar. */}
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
                 {props.menuItems.map((item) => (
                     <li key={item}>
-                        <a href="#" className="block py-2 px-3 rounded md:p-0" aria-current="page"> {item} </a>
+                        <a href="#" className="text-2xl hover:font-semibold block py-2 px-3 rounded md:p-0" aria-current="page"> {item.toLocaleLowerCase()} </a>
                     </li>
                 ))}
             </ul>
