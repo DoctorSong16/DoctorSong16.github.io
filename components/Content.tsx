0436ec85cdcb80cf95981989bb76bd63566f4a26
Ashwin Narayan <ashwinnarayan1994@gmail.com>
2024-11-11 11:56:56 +0800

    Moved post preview to new component, added styling

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,5 +1,6 @@
 import { ArticleItem } from '@/types'
 import React from 'react'
+import PostPreviewCard from './PostPreviewCard';
 
 type Props = {
     postData: ArticleItem[];
@@ -7,13 +8,9 @@ type Props = {
 
 export default function Content(props: Props) {
   return (
-    <div className='max-w-screen-xl mx-auto p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 font-ibarraRealNova'>
+    <div className='max-w-screen-xl mx-auto p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 font-ibarraRealNova'>
         {props.postData.map((post) => (
-            <div key={post.id} className='flex flex-col items-center justify-center mx-auto w-11/12'>
-                <h2 className='text-2xl'>{post.title}</h2>
-                <p>{post.date}</p>
-                <p>{post.summary}</p>
-            </div>
+            <PostPreviewCard key={post.id} postData={post}/>
         ))}
     </div>
   )
--- components/PostPreviewCard.tsx
+++ components/PostPreviewCard.tsx
@@ -7,6 +7,10 @@ type Props = {
 
 export default function PostPreviewCard(props: Props) {
   return (
-    <div>PostPreviewCard - {props.postData.id}</div>
+    <div className='transition duration-300 flex flex-col mx-auto w-11/12 items-center shadow-md rounded-md hover:rounded-lg hover:shadow-lg p-4'>
+        <h2 className='text-2xl font-semibold'>{props.postData.title}</h2>
+        <p>{props.postData.date}</p>
+        <p>{props.postData.summary}</p>
+    </div>
   )
 }
