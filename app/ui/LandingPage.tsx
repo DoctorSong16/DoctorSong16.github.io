a5f121703b98350b4bb2b3af36a6bee3f8a43ea5
Ashwin Narayan <ashwinnarayan1994@gmail.com>
2024-11-19 15:25:42 +0800

    Text looks better at all sizes now

--- app/ui/LandingPage.tsx
+++ app/ui/LandingPage.tsx
@@ -9,10 +9,10 @@ export default function LandingPage() {
             <img src="/dundu.jpeg" className='object-contain m-2 shadow-lg border-8 border-foreground rounded-[24px]' alt="dundu" />
         </div>
         <div className='w-11/12 md:w-1/2 flex flex-col p-8'>
-            <h1 className='text-4xl md:text-2xl'>
+            <h1 className='text-4xl'>
                 Hi!, I&apos;m Pratibha
             </h1>
-            <p className='text-xl md:text-sm lg:text-xl'>
+            <p className='text-xl md:text-sm lg:text-xl xl:text-2xl'>
                 Welcome to my personal blog! Lorem ipsum dolor sit amet, consectetur adipiscing elit
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio explicabo odio eum officia adipisci. Accusantium voluptates eius voluptatum at. Corporis nam odio at quod ea earum aperiam numquam asperiores suscipit!
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam quos reiciendis alias officiis nam, commodi nihil perspiciatis tempore consectetur dolore, animi at accusamus quaerat molestiae, quo dignissimos recusandae maxime accusantium?
