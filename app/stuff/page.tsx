c4f9f6a4064b29e29855eeffbd02fe06e18b495e
Ashwin Narayan <ashwinnarayan1994@gmail.com>
2024-11-25 12:28:44 +0800

    Added placeholder pages for navbar links

---
+++ app/stuff/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// type Props = {}
+
+export default function page() {
+  return (
+    <div className="flex flex-col max-w-screen-md mx-auto text-center min-h-screen font-ibarraRealNova items-center">
+        <p className='my-auto text-2xl font-semibold'>
+            204 | Some stuff is coming here soon, stay tuned!
+        </p>
+    </div>
+  )
+}
